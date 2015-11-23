d45ec9882fd34efe8e4902d764fc530799466752
aseembhandari <bhandariaseem@gmail.com>
2015-11-22 22:37:37 -0800

    Even Odd program

---
+++ src/org/java/learning/numbers/EvenOdd.java
@@ -0,0 +1,12 @@
+package org.java.learning.numbers;
+
+public class EvenOdd {
+	public static void main(String[] args) {
+		int number = 10;
+		if (number % 2 == 0) {
+			System.out.print("number is even");
+		} else {
+			System.out.println("number is odd");
+		}
+	}
+}
