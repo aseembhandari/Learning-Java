e257dfc43a37cb845b43aa08060ed419d3d62ada
aseembhandari <bhandariaseem@gmail.com>
2015-11-24 23:19:37 -0800

    if else in Java

---
+++ src/org/java/learning/numbers/IfElse.java
@@ -0,0 +1,17 @@
+package org.java.learning.numbers;
+/**
+ * This program demonstrates conditional statement in Java
+ * */
+public class IfElse {
+
+	public static void main(String args[]) {
+		int test = 45;
+
+		if (test < 9) {
+			System.out.println("Number is less than 9");
+		} else {
+			System.out.println("Number is greater than 9");
+		}
+	}
+
+}
