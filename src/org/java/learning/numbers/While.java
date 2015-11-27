b123fff8effc32cc4ac688657c946e508cf8fb8a
aseembhandari <bhandariaseem@gmail.com>
2015-11-27 00:31:51 -0800

    While Loop

---
+++ src/org/java/learning/numbers/While.java
@@ -0,0 +1,17 @@
+package org.java.learning.numbers;
+/**
+ * This program demonstrates the working of While loop in Java
+ * */
+public class While {
+	public static void main(String[] args) {
+		
+	int counter;
+	counter=0;
+	while(counter<10)
+
+	{
+		System.out.println(counter);
+		counter++;
+	}
+}
+}
